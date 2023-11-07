234017e1c03d9d07ce338cc5b33aa5cc718514e4
Vadim Zaets <79648897+VadimZaets@users.noreply.github.com>
2023-11-07 13:38:36 +0200

    add OneSignal

--- MyApp/index.js
+++ MyApp/index.js
@@ -1,10 +1,23 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import Appsflyer from 'react-native-appsflyer';
+// import Appsflyer from 'react-native-appsflyer';
 
 
-import { useEffect } from 'react';
+// import OneSignal from 'react-native-onesignal';
+
+// OneSignal.setAppId('YOUR_ONESIGNAL_APP_ID');
+
+// OneSignal.setNotificationOpenedHandler((openedEvent) => {
+//   console.log(openedEvent);
+// });
+
+// OneSignal.setNotificationReceivedHandler((receivedEvent) => {
+//   console.log(receivedEvent);
+// });
+
+// firebase знаходиться у файлі firebaseConfig
+
 // Appsflyer.initSdk(
 // 	{
 // 	  devKey: 'YOUR_APPSFLYER_DEV_KEY', 
